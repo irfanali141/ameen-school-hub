bab0efc7da572b73e982368b8bffacd6f6418644
irfanali141 <irfanali141@gmail.com>
2026-03-18 17:14:05 +0500

    Hardcode Supabase URL/anon key

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const SUPABASE_URL = "https://ryxjptmkicvupaqprdfa.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_TQBixCySY8LJAi_qVN9RHw_cIWleR7q";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
